284f1f6ce938dd4d18c5f68f3a6b8672ee8738eb
smmonirhasan92 <smmonirhasan92@github.com>
2026-07-18 18:36:04 +0600

    Fix checkout.js caching issue

--- customer/index.php
+++ customer/index.php
@@ -178,7 +178,8 @@
     <script src="../assets/js/main.js"></script>
     <script src="../assets/js/notifications.js"></script>
     <script src="../assets/js/animations.js"></script>
-    <script src="../assets/js/checkout.js"></script>
+    <!-- Checkout Handling (Version updated to clear browser cache) -->
+    <script src="../assets/js/checkout.js?v=2.0"></script>
 
     <script>
         // Helper to optimize Unsplash images for storefront speed
